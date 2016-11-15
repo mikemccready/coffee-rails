3bf8ec0436f883b56708d6fdd4e0dcd3d5d9e62f
Mike McCready <mikem@inspero.net>
2016-11-15 14:30:02 -0800

    add new coffee maker form

--- app/controllers/coffee_makers_controller.rb
+++ app/controllers/coffee_makers_controller.rb
@@ -1,2 +1,19 @@
 class CoffeeMakersController < ApplicationController
+	def show
+		@coffee_maker = CoffeeMaker.find(params[:id])
+	end
+	def new
+	end
+
+	def create
+		@coffee_maker = CoffeeMaker.new(coffee_maker_params)
+
+		@coffee_maker.save
+		redirect_to @coffee_maker
+	end
+
+	private
+		def coffee_maker_params
+			params.require(:coffee_maker).permit(:product_type, :water_line_compatible, :sku)
+		end
 end
