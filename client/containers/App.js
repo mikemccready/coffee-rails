65dddda72c49e147bdd68c654319fcd6ebe97572
Mike McCready <mikem@inspero.net>
2016-11-19 12:06:25 -0800

    fetching machine data from api, rendering to App.js

--- client/containers/App.js
+++ client/containers/App.js
@@ -2,7 +2,45 @@
 import React from 'react';
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			machines: []
+		}
+		this.getMachines = this.getMachines.bind(this);
+	}
+
+	componentDidMount() {
+		this.getMachines();
+	}
+
+	getMachines() {
+		const that = this;
+		fetch('http://localhost:3000/coffee_makers/')  
+		  .then(response => {  
+	      if (response.status !== 200) {  
+	        console.log('Error. Status Code: ' + response.status);  
+	        return;  
+	      }
+	      response.json().then(data => {  
+	        that.setState({machines: data})  
+	      });  
+		  })  
+		  .catch(err => {  
+		    console.log('Fetch Error :-S', err);  
+		  });
+	}
+
 	render() {
-		return <div>Hello</div>
+		const machines = this.state.machines.map((machine, i) => {
+			return <div key={i}>{machine.product_type}</div>
+		})
+
+		return(
+			<div>
+				<h3>Machines</h3>
+				{ machines }
+			</div>
+		)
 	}
 }
