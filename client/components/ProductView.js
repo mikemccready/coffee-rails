8957ab3c540475c38496954906806cd2ba7e42c0
Mike McCready <mikem@inspero.net>
2016-11-27 13:43:28 -0800

    render route extension to product view component

--- client/components/ProductView.js
+++ client/components/ProductView.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-export default class ProductView extends extends React.Component {
-	
+export default class ProductView extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			product: ''
+		}
+	}
+
+	componentWillMount() {
+		let product = window.location.pathname;
+		this.setState({product: product})
+	}
+
+	componentDidMount() {
+		// this.getProductData();
+	}
+
+	render() {
+		console.log(window.location)
+		return <div>{this.state.product}</div>
+	}
 }
