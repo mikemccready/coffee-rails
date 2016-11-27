d40ad13598b80606d43fc462dd5c810171fe6548
Mike McCready <mikem@inspero.net>
2016-11-27 13:42:56 -0800

    add link to specific product view

--- client/components/ProductList.js
+++ client/components/ProductList.js
@@ -1,25 +1,86 @@
 // ProductList.js
 import React from 'react';
+import { Link } from 'react-router';
 
-export default function ProductList(props) {
-	const machines = props.machineData.map((machine, i) => {
-		return <div key={i}>{machine.product_type}<br/><br/></div>
-	})
+export default class ProductList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			machineData: [],
+			podData: []
+		}
+		this.getMachineData = this.getMachineData.bind(this);
+		this.getPodData = this.getPodData.bind(this);
+	}
 
-	const pods = props.podData.map((pod, i) => {
-		return 	<div key={i}>
-							{pod.product_type}<br/>
-							{pod.coffee_flavor}
-							<br/><br/>
-						</div>
-	})
+	componentDidMount() {
+		this.getMachineData();
+		this.getPodData();
+	}
 
-	return(
-		<div>
-			<h3>Machines</h3>
-			{ machines }
-			<h3>Pods</h3>
-			{ pods }
-		</div>
-	)
+	getMachineData() {
+		const that = this;
+		fetch('http://localhost:3000/coffee_makers/')  
+		  .then(response => {  
+	      if (response.status !== 200) {  
+	        console.log('Error. Status Code: ' + response.status);  
+	        return;  
+	      }
+	      response.json().then(data => { 
+	        that.setState({machineData: data})  
+	      });  
+		  })  
+		  .catch(err => {  
+		    console.log('Fetch Error :-S', err);  
+		  });
+	}
+
+	getPodData() {
+		const that = this;
+		fetch('http://localhost:3000/coffee_pods/')  
+		  .then(response => {  
+	      if (response.status !== 200) {  
+	        console.log('Error. Status Code: ' + response.status);  
+	        return;  
+	      }
+	      response.json().then(data => { 
+	        that.setState({podData: data})  
+	      });  
+		  })  
+		  .catch(err => {  
+		    console.log('Fetch Error :-S', err);  
+		  });
+	}
+
+	render() {
+		const machines = this.state.machineData.map((machine, i) => {
+			const productLink = '/products/coffee-makers/' + machine.id;
+			return 	<div key={i}>
+								<Link to={productLink}>
+									{machine.product_type}
+									<br/><br/>
+								</Link>
+							</div>
+		})
+
+		const pods = this.state.podData.map((pod, i) => {
+			const productLink = '/products/coffee-pods/' + pod.id;
+			return 	<div key={i}>
+								<Link to={productLink}>
+									{pod.product_type}<br/>
+									{pod.coffee_flavor}
+									<br/><br/>
+								</Link>
+							</div>
+		})
+
+		return(
+			<div>
+				<h3>Machines</h3>
+				{ machines }
+				<h3>Pods</h3>
+				{ pods }
+			</div>
+		)	
+	}
 }
