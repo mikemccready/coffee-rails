12bc55f4967e4071de3e4ffcbb0b926159f23e43
Mike McCready <mikem@inspero.net>
2016-11-27 13:40:25 -0800

    setup app routes

--- client/index.js
+++ client/index.js
@@ -1,11 +1,22 @@
 // index.js
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, Link, browserHistory } from 'react-router';
+import { Router, Route, IndexRedirect, Link, browserHistory } from 'react-router';
 
-import App from './containers/App'
+import App from './containers/App';
+import ProductList from './components/ProductList';
+import ProductView from './components/ProductView';
 
 // attach point
 const appRoot = document.getElementById('app');
 
-render(<App />, appRoot);
+render((
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			<IndexRedirect to="products"/>
+			<Route path="products" component={ProductList}/>
+			<Route path="/products/coffee-pods/:id" component={ProductView}/>
+			<Route path="/products/coffee-makers/:id" component={ProductView}/>
+		</Route>
+	</Router>
+), appRoot);
