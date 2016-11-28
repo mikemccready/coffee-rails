39d7613cd800caee050cf892a35d103f15d91588
Mike McCready <mikem@inspero.net>
2016-11-28 13:12:27 -0800

    list related products, and links to update product

--- client/components/ProductView.js
+++ client/components/ProductView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class ProductView extends React.Component {
 	constructor(props) {
@@ -9,16 +10,13 @@ export default class ProductView extends React.Component {
 			productData: {}
 		}
 		this.getProductData = this.getProductData.bind(this);
+		this.updateProduct = this.updateProduct.bind(this);
 	}
 
 	componentWillMount() {
 		const product = this.props.params.product;
 		const id = this.props.params.id
-		this.setState({product: product, id: id})
-	}
-
-	componentDidMount() {
-		this.getProductData();
+		this.setState({product: product, id: id}, this.getProductData);
 	}
 
 	getProductData() {
@@ -38,8 +36,44 @@ export default class ProductView extends React.Component {
 		  });
 	}
 
+	updateProduct(e, id, product, data) {
+		const newProduct = product;
+		const newId = id
+		this.setState({product: newProduct, id: newId, productData: data}, this.getProductData)
+	}	
+
 	render() {
-		console.log(this.state.productData)
-		return <div>{this.state.productData.product_title}</div>
+		let relatedProducts;
+
+		if (this.state.productData.coffee_pods) {
+			relatedProducts = this.state.productData.coffee_pods.map((product, i) => {
+				const productLink = '/products/coffee_pods/' + product.id;
+				return 	<div key={i}>
+									<Link to={productLink} onClick={(e) => {this.updateProduct(e, product.id, 'coffee_pods', product)}}>
+										{product.product_title}<br/>
+										{product.pack_size}
+										<br/><br/>
+									</Link>
+								</div>
+			})
+		}	else if (this.state.productData.coffee_makers) {
+			relatedProducts = this.state.productData.coffee_makers.map((product, i) => {
+				const productLink = '/products/coffee_makers/' + product.id;
+				return 	<div key={i}>
+									<Link to={productLink} onClick={(e) => {this.updateProduct(e, product.id, 'coffee_makers', product)}}>
+										{product.product_title}<br/>
+										{product.sku}
+										<br/><br/>
+									</Link>
+								</div>
+			})
+		}
+
+		return(
+			<div>
+				{this.state.productData.product_title}
+				{relatedProducts}
+			</div>
+		)
 	}
 }
