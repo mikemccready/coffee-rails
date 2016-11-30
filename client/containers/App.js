827356fdec2eaace496ba080f643b0a48cd2bdf2
Mike McCready <mikem@inspero.net>
2016-11-29 23:41:02 -0800

    move product data get functions to app

--- client/containers/App.js
+++ client/containers/App.js
@@ -6,11 +6,56 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
 
 class AppComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.getMachineData = this.getMachineData.bind(this);
+		this.getPodData = this.getPodData.bind(this);
+	}
+
+	componentDidMount() {
+		this.getMachineData();
+		this.getPodData();
+	}
+
+	getMachineData() {
+		const that = this;
+		fetch('http://localhost:3000/coffee_makers/')  
+		  .then(response => {  
+	      if (response.status !== 200) {  
+	        console.log('Error. Status Code: ' + response.status);  
+	        return;  
+	      }
+	      response.json().then(data => { 
+	        that.props.setMachineData(data);  
+	      });  
+		  })  
+		  .catch(err => {  
+		    console.log('Fetch Error :-S', err);  
+		  });
+	}
+
+	getPodData() {
+		const that = this;
+		fetch('http://localhost:3000/coffee_pods/')  
+		  .then(response => {  
+	      if (response.status !== 200) {  
+	        console.log('Error. Status Code: ' + response.status);  
+	        return;  
+	      }
+	      response.json().then(data => { 
+	        that.props.setPodData(data);
+	      });  
+		  })  
+		  .catch(err => {  
+		    console.log('Fetch Error :-S', err);  
+		  });
+	}
+
 	render() {
 		return(
 			<div>
 				<nav>
-					<Link to="/products">Products</Link>
+					<Link to="/">Products</Link>
 				</nav>
 				<div className="main-container">
 					{React.cloneElement(this.props.children, this.props)}
