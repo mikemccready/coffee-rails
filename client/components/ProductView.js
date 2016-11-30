638a63755608ce51389d1da06c85012dd16401d0
Mike McCready <mikem@inspero.net>
2016-11-30 09:06:55 -0800

    refactor product view to use selectedProduct data from store

--- client/components/ProductView.js
+++ client/components/ProductView.js
@@ -2,18 +2,16 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default function ProductView(props) {
-	const id = props.params.id;
-	const product = props.params.product;
-
 	let relatedProducts;
-	let productData;
+	let productData = props.selectedProduct;
+	let product = props.params.product;
 
 	if (product === 'coffee_makers') {
-		productData = props.machineData[id - 1];
 		relatedProducts = productData.coffee_pods.map((product, i) => {
 			const productLink = '/products/coffee_pods/' + product.id;
+			const productData = props.podData[product.id - 1];
 			return 	<div key={i}>
-								<Link to={productLink}>
+								<Link to={productLink} onClick={() => {props.setSelectedProduct(productData)}}>
 									{product.product_title}<br/>
 									{product.pack_size}
 									<br/><br/>
@@ -21,11 +19,11 @@ export default function ProductView(props) {
 							</div>
 		});
 	}	else if (product === 'coffee_pods') {
-		productData = props.podData[id - 1];
 		relatedProducts = productData.coffee_makers.map((product, i) => {
 			const productLink = '/products/coffee_makers/' + product.id;
+			const productData = props.machineData[product.id - 1];
 			return 	<div key={i}>
-								<Link to={productLink}>
+								<Link to={productLink} onClick={() => {props.setSelectedProduct(productData)}}>
 									{product.product_title}<br/>
 									{product.sku}
 									<br/><br/>
@@ -37,7 +35,7 @@ export default function ProductView(props) {
 	return(
 		<div>
 			<img src={productData.image_url} />
-			<h2>{productData.product_title}</h2>
+			<h2>{productData.product_title} ({productData.sku})</h2>
 			<h4>Owners also purchased</h4>
 			{relatedProducts}
 		</div>
