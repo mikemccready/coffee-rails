19d2164c48aa91bfa364c40f2c646e8b0b790c05
Mike McCready <mikem@inspero.net>
2016-11-30 09:08:37 -0800

    add selected product reducer to root reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,10 +4,12 @@ import { routerReducer } from 'react-router-redux';
 
 import machineData from './coffee_makers';
 import podData from './coffee_pods';
+import selectedProduct from './selected_product';
 
 const rootReducer = combineReducers({
 	machineData,
 	podData,
+	selectedProduct,
 	routing: routerReducer
 });
 
